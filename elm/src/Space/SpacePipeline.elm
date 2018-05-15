0d1a662c5c94e7cb34c359a7f3f488d859946d68
Mark Huang <mhuang@pivotal.io>
2018-05-15 17:22:35 -0400

    WIP: use dashboard layout for spaces view

--- elm/src/Space/SpacePipeline.elm
+++ elm/src/Space/SpacePipeline.elm
@@ -1,12 +1,8 @@
 module SpacePipeline exposing (Flags, Model, Msg, init, update, view, subscriptions, changeToPipeline)
 
 import Concourse
-import Concourse.BuildStatus
 import Concourse.Job
 import Concourse.Resource
-import Dict exposing (Dict)
-import Graph exposing (Graph)
-import Grid exposing (Grid)
 import Html exposing (Html)
 import Html.Attributes exposing (class, classList, href, rowspan, attribute)
 import Html.Events exposing (onMouseOver, onMouseLeave)
@@ -14,6 +10,7 @@ import Http
 import SpaceRoutes
 import Task
 import Time exposing (Time)
+import SpacePreview
 
 
 type alias Model =
@@ -25,27 +22,6 @@ type alias Model =
     , turbulenceImgSrc : String
     , selectedResourceSpace : ( String, String )
     }
-
-
-type alias ByName a =
-    Dict String a
-
-
-type Node
-    = JobNode Concourse.SpaceJob
-    | InputNode
-        { resourceName : String
-        , dependentJob : Concourse.SpaceJob
-        }
-    | OutputNode
-        { resourceName : String
-        , upstreamJob : Concourse.SpaceJob
-        }
-    | ConstrainedInputNode
-        { resourceName : String
-        , dependentJob : Concourse.SpaceJob
-        , upstreamJob : Maybe Concourse.SpaceJob
-        }
 
 
 type alias Ports =
@@ -156,8 +132,7 @@ view : Model -> Html Msg
 view model =
     Html.div [ class "pipeline-content" ]
         [ viewResources model.resources
-        , Html.div [ class "pipeline-grid" ]
-            [ viewGrid (Grid.fromGraph <| initGraph model.jobs) model.selectedResourceSpace ]
+        , SpacePreview.view model.jobs model.selectedResourceSpace
         ]
 
 
@@ -168,216 +143,21 @@ viewResources resources =
 
 viewResource : Concourse.SpaceResource -> Html Msg
 viewResource resource =
-    Html.div [ class "resource" ]
-        [ Html.div [ class "resource-name" ] [ Html.text resource.name ]
-        , Html.div [ class "resource-spaces" ] <| List.map (\space -> viewResourceSpace ( resource.name, space )) resource.spaces
-        ]
+    if List.length resource.spaces > 1 then
+        Html.div [ class "resource" ]
+            [ Html.div [ class "resource-name" ] [ Html.text resource.name ]
+            , Html.div [ class "resource-spaces" ] <| List.map (\space -> viewResourceSpace ( resource.name, space )) resource.spaces
+            ]
+    else
+        Html.div [ class "resource" ]
+            [ Html.div
+                [ class "resource-name space-mouseover", onMouseOver (ResourceHoverMsg ( resource.name, "default" )), onMouseLeave (ResourceHoverMsg ( "", "" )) ]
+                [ Html.text resource.name ]
+            ]
 
 
 viewResourceSpace : ( String, String ) -> Html Msg
 viewResourceSpace ( resource, space ) =
     Html.div
-        [ class "resource-space", onMouseOver (ResourceHoverMsg ( resource, space )), onMouseLeave (ResourceHoverMsg ( "", "" )) ]
+        [ class "resource-space space-mouseover", onMouseOver (ResourceHoverMsg ( resource, space )), onMouseLeave (ResourceHoverMsg ( "", "" )) ]
         [ Html.text space ]
-
-
-viewGrid : Grid Node () -> ( String, String ) -> Html Msg
-viewGrid grid resourceSpace =
-    case grid of
-        Grid.Cell { node } ->
-            viewNode node resourceSpace
-
-        Grid.Serial prev next ->
-            Html.div [ class "serial-grid" ]
-                (viewSerial prev resourceSpace ++ viewSerial next resourceSpace)
-
-        Grid.Parallel grids ->
-            Html.div [ class "space-parallel-grid" ] <|
-                List.map (\grid -> viewGrid grid resourceSpace) grids
-
-        Grid.End ->
-            Html.text ""
-
-
-viewSerial : Grid Node () -> ( String, String ) -> List (Html Msg)
-viewSerial grid resourceSpace =
-    case grid of
-        Grid.Serial prev next ->
-            viewSerial prev resourceSpace ++ viewSerial next resourceSpace
-
-        _ ->
-            [ viewGrid grid resourceSpace ]
-
-
-viewNode : Graph.Node Node -> ( String, String ) -> Html Msg
-viewNode { id, label } resourceSpace =
-    let
-        idAttr =
-            Html.Attributes.id ("node-" ++ toString id)
-    in
-        case label of
-            JobNode job ->
-                Html.div [ class "node", idAttr ]
-                    [ Html.div
-                        [ classList [ ( "job", True ), ( "paused", job.paused ) ] ]
-                        [ Html.a [ class "job-name" ]
-                            [ Html.text job.name
-                            ]
-                        , Html.div [ class "combination-container" ] <|
-                            List.map (\combination -> viewJobCombination combination resourceSpace job) job.combintations
-                        ]
-                    ]
-
-            InputNode { resourceName } ->
-                Html.div [ class "node input", idAttr ] [ Html.text "" ]
-
-            ConstrainedInputNode { resourceName } ->
-                Html.div [ class "node input constrained", idAttr ] [ Html.text "" ]
-
-            OutputNode { resourceName } ->
-                Html.div [ class "node output", idAttr ] [ Html.text "" ]
-
-
-viewJobCombination : Concourse.SpaceJobCombination -> ( String, String ) -> Concourse.SpaceJob -> Html Msg
-viewJobCombination jobCombination ( resource, space ) job =
-    let
-        buildStatus =
-            case ( job.paused, jobCombination.finishedBuild, jobCombination.nextBuild ) of
-                ( True, _, _ ) ->
-                    "paused"
-
-                ( False, Nothing, Nothing ) ->
-                    "no-builds"
-
-                ( False, Just build, Nothing ) ->
-                    Concourse.BuildStatus.show build.status
-
-                ( False, Nothing, Just build ) ->
-                    "no-builds started"
-
-                ( False, Just fb, Just nb ) ->
-                    Concourse.BuildStatus.show fb.status ++ " started"
-
-        active =
-            (String.isEmpty space) || Dict.get resource jobCombination.combination == Just space
-    in
-        Html.div
-            [ class "job-combination", attribute "data-tooltip" (jobCombinationTooltip job jobCombination) ]
-            [ Html.a
-                [ classList
-                    [ ( "job-combination-status", True )
-                    , ( buildStatus, True ) -- FIXME: check for next build
-                    , ( "inactive", not active )
-                    ]
-                , href <| viewJobCombinationLink jobCombination
-                ]
-                []
-            ]
-
-
-viewJobCombinationLink : Concourse.SpaceJobCombination -> String
-viewJobCombinationLink jobCombination =
-    case ( jobCombination.finishedBuild, jobCombination.nextBuild ) of
-        ( _, Just nb ) ->
-            SpaceRoutes.buildRoute nb
-
-        ( Just fb, Nothing ) ->
-            SpaceRoutes.buildRoute fb
-
-        ( Nothing, Nothing ) ->
-            "/"
-
-
-jobCombinationTooltip : Concourse.SpaceJob -> Concourse.SpaceJobCombination -> String
-jobCombinationTooltip job jobCombination =
-    let
-        space =
-            \name combination -> name ++ " [" ++ (Maybe.withDefault "" <| Dict.get name combination) ++ "] \n"
-
-        inputs =
-            List.map (\input -> "⬇ " ++ (space input.name jobCombination.combination)) job.inputs
-
-        outputs =
-            List.map (\output -> "⬆ " ++ (space output.name jobCombination.combination)) job.outputs
-    in
-        String.concat << List.concat <| [ inputs, outputs ]
-
-
-initGraph : List Concourse.SpaceJob -> Graph Node ()
-initGraph jobs =
-    let
-        jobNodes =
-            List.map JobNode jobs
-
-        jobsByName =
-            List.foldl (\job dict -> Dict.insert job.name job dict) Dict.empty jobs
-
-        resourceNodes =
-            List.concatMap (jobResourceNodes jobsByName) jobs
-
-        graphNodes =
-            List.indexedMap Graph.Node (List.concat [ jobNodes, resourceNodes ])
-    in
-        Graph.fromNodesAndEdges
-            graphNodes
-            (List.concatMap (nodeEdges graphNodes) graphNodes)
-
-
-jobResourceNodes : ByName Concourse.SpaceJob -> Concourse.SpaceJob -> List Node
-jobResourceNodes jobs job =
-    List.concatMap (inputNodes jobs job) job.inputs
-        ++ List.concatMap (outputNodes job) job.outputs
-
-
-inputNodes : ByName Concourse.SpaceJob -> Concourse.SpaceJob -> Concourse.JobInput -> List Node
-inputNodes jobs job { resource, passed } =
-    if List.isEmpty passed then
-        [ InputNode { resourceName = resource, dependentJob = job } ]
-    else
-        List.map (constrainedInputNode jobs resource job) passed
-
-
-outputNodes : Concourse.SpaceJob -> Concourse.JobOutput -> List Node
-outputNodes job { resource } =
-    [ OutputNode { resourceName = resource, upstreamJob = job } ]
-
-
-constrainedInputNode : ByName Concourse.SpaceJob -> String -> Concourse.SpaceJob -> String -> Node
-constrainedInputNode jobs resourceName dependentJob upstreamJobName =
-    ConstrainedInputNode
-        { resourceName = resourceName
-        , dependentJob = dependentJob
-        , upstreamJob = Dict.get upstreamJobName jobs
-        }
-
-
-nodeEdges : List (Graph.Node Node) -> Graph.Node Node -> List (Graph.Edge ())
-nodeEdges allNodes { id, label } =
-    case label of
-        JobNode _ ->
-            []
-
-        InputNode { dependentJob } ->
-            [ Graph.Edge id (jobId allNodes dependentJob) () ]
-
-        ConstrainedInputNode { dependentJob, upstreamJob } ->
-            Graph.Edge id (jobId allNodes dependentJob) ()
-                :: case upstreamJob of
-                    Just upstream ->
-                        [ Graph.Edge (jobId allNodes upstream) id () ]
-
-                    Nothing ->
-                        []
-
-        OutputNode { upstreamJob } ->
-            [ Graph.Edge (jobId allNodes upstreamJob) id () ]
-
-
-jobId : List (Graph.Node Node) -> Concourse.SpaceJob -> Int
-jobId nodes job =
-    case List.filter ((==) (JobNode job) << .label) nodes of
-        { id } :: _ ->
-            id
-
-        [] ->
-            Debug.crash "impossible: job index not found"
---
+++ elm/src/Space/spacePreview.elm
@@ -0,0 +1,164 @@
+module SpacePreview exposing (view)
+
+import Concourse
+import Concourse.BuildStatus
+import Debug
+import Dict exposing (Dict)
+import Html exposing (Html)
+import Html.Attributes exposing (attribute, class, classList, href)
+import SpaceRoutes
+
+
+view : List Concourse.SpaceJob -> ( String, String ) -> Html msg
+view jobs resourceSpace =
+    let
+        groups =
+            jobGroups jobs
+    in
+        Html.div [ class "pipeline-grid" ] <|
+            List.map
+                (\jobs ->
+                    List.map (viewJob resourceSpace) jobs
+                        |> Html.div [ class "space-parallel-grid" ]
+                )
+                (Dict.values groups)
+
+
+viewJob : ( String, String ) -> Concourse.SpaceJob -> Html msg
+viewJob resourceSpace job =
+    Html.div [ class "node" ]
+        [ Html.div
+            [ classList [ ( "job", True ), ( "paused", job.paused ) ] ]
+            [ Html.a [ class "job-name" ]
+                [ Html.text job.name
+                ]
+            , Html.div [ class "combination-container" ] <|
+                List.map (\combination -> viewJobCombination combination resourceSpace job) job.combintations
+            ]
+        ]
+
+
+viewJobCombination : Concourse.SpaceJobCombination -> ( String, String ) -> Concourse.SpaceJob -> Html msg
+viewJobCombination jobCombination ( resource, space ) job =
+    let
+        buildStatus =
+            case ( job.paused, jobCombination.finishedBuild, jobCombination.nextBuild ) of
+                ( True, _, _ ) ->
+                    "paused"
+
+                ( False, Nothing, Nothing ) ->
+                    "no-builds"
+
+                ( False, Just build, Nothing ) ->
+                    Concourse.BuildStatus.show build.status
+
+                ( False, Nothing, Just build ) ->
+                    "no-builds started"
+
+                ( False, Just fb, Just nb ) ->
+                    Concourse.BuildStatus.show fb.status ++ " started"
+
+        active =
+            (String.isEmpty space) || Dict.get resource jobCombination.combination == Just space
+    in
+        Html.div
+            [ class "job-combination", attribute "data-tooltip" (jobCombinationTooltip job jobCombination) ]
+            [ Html.a
+                [ classList
+                    [ ( "job-combination-status", True )
+                    , ( buildStatus, True ) -- FIXME: check for next build
+                    , ( "inactive", not active )
+                    ]
+                , href <| viewJobCombinationLink jobCombination
+                ]
+                []
+            ]
+
+
+viewJobCombinationLink : Concourse.SpaceJobCombination -> String
+viewJobCombinationLink jobCombination =
+    case ( jobCombination.finishedBuild, jobCombination.nextBuild ) of
+        ( _, Just nb ) ->
+            SpaceRoutes.buildRoute nb
+
+        ( Just fb, Nothing ) ->
+            SpaceRoutes.buildRoute fb
+
+        ( Nothing, Nothing ) ->
+            "/"
+
+
+jobCombinationTooltip : Concourse.SpaceJob -> Concourse.SpaceJobCombination -> String
+jobCombinationTooltip job jobCombination =
+    let
+        space =
+            \name combination -> name ++ " [" ++ (Maybe.withDefault "" <| Dict.get name combination) ++ "] \n"
+
+        inputs =
+            List.map (\input -> "⬇ " ++ (space input.name jobCombination.combination)) job.inputs
+
+        outputs =
+            List.map (\output -> "⬆ " ++ (space output.name jobCombination.combination)) job.outputs
+    in
+        String.concat << List.concat <| [ inputs, outputs ]
+
+
+jobGroups : List Concourse.SpaceJob -> Dict Int (List Concourse.SpaceJob)
+jobGroups jobs =
+    let
+        jobLookup =
+            jobByName <| List.foldl (\job byName -> Dict.insert job.name job byName) Dict.empty jobs
+    in
+        Dict.foldl
+            (\jobName depth byDepth ->
+                Dict.update depth
+                    (\jobsA ->
+                        Just (jobLookup jobName :: Maybe.withDefault [] jobsA)
+                    )
+                    byDepth
+            )
+            Dict.empty
+            (jobDepths jobs Dict.empty)
+
+
+jobByName : Dict String Concourse.SpaceJob -> String -> Concourse.SpaceJob
+jobByName jobs job =
+    case Dict.get job jobs of
+        Just a ->
+            a
+
+        Nothing ->
+            Debug.crash "impossible"
+
+
+jobDepths : List Concourse.SpaceJob -> Dict String Int -> Dict String Int
+jobDepths jobs dict =
+    case jobs of
+        [] ->
+            dict
+
+        job :: otherJobs ->
+            let
+                passedJobs =
+                    List.concatMap .passed job.inputs
+            in
+                case List.length passedJobs of
+                    0 ->
+                        jobDepths otherJobs <| Dict.insert job.name 0 dict
+
+                    _ ->
+                        let
+                            passedJobDepths =
+                                List.map (\passedJob -> Dict.get passedJob dict) passedJobs
+                        in
+                            if List.member Nothing passedJobDepths then
+                                jobDepths (List.append otherJobs [ job ]) dict
+                            else
+                                let
+                                    depths =
+                                        List.map (\depth -> Maybe.withDefault 0 depth) passedJobDepths
+
+                                    maxPassedJobDepth =
+                                        Maybe.withDefault 0 <| List.maximum depths
+                                in
+                                    jobDepths otherJobs <| Dict.insert job.name (maxPassedJobDepth + 1) dict
