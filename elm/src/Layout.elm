d997d7e0858b0893c36aa725978adb859c4832d6
Jamie Klassen <cklassen@pivotal.io>
2018-08-10 18:02:03 -0400

    starting to pull TopBar into Layout
    concourse/concourse#2484
    
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- elm/src/Layout.elm
+++ elm/src/Layout.elm
@@ -1,13 +1,21 @@
 port module Layout exposing (Flags, Model, Msg, init, locationMsg, subscriptions, update, view)
 
+import Concourse
+import Concourse.Pipeline
+import Concourse.User
 import Favicon
 import Html exposing (Html)
-import Html.Attributes as Attributes exposing (class, id)
+import Html.Attributes as Attributes exposing (attribute, class, classList, disabled, href, id, style)
+import Html.Events exposing (onClick)
+import Http
+import LoginRedirect
 import Navigation
 import Routes
 import SubPage
+import StrictEvents exposing (onLeftClickOrShiftLeftClick)
 import Task exposing (Task)
 import TopBar
+import Time
 
 
 port newUrl : (String -> msg) -> Sub msg
@@ -47,6 +55,7 @@ type alias Model =
     , notFoundImgSrc : String
     , csrfToken : String
     , route : Routes.ConcourseRoute
+    , pipeline : Maybe Concourse.Pipeline
     }
 
 
@@ -64,7 +73,12 @@ type Msg
     | ModifyUrl String
     | SaveToken String
     | LoadToken
+    | LogIn
+    | LogOut
+    | LoggedOut (Result Http.Error ())
     | TokenReceived (Maybe String)
+    | FetchPipeline Concourse.PipelineIdentifier
+    | PipelineFetched (Result Http.Error Concourse.Pipeline)
 
 
 init : Flags -> Navigation.Location -> ( Model, Cmd Msg )
@@ -88,7 +102,25 @@ init flags location =
             SubPage.init { turbulencePath = flags.turbulenceImgSrc, csrfToken = flags.csrfToken } route
 
         ( topModel, topCmd ) =
-            TopBar.init route
+            let
+                pid =
+                    extractPidFromRoute route.logical
+            in
+                ( { route = route
+                  , userState = TopBar.UserStateUnknown
+                  , userMenuVisible = False
+                  }
+                , case pid of
+                    Nothing ->
+                        Task.attempt (TopMsg TopBar.UserFetched) Concourse.User.fetchUser
+
+                    Just pid ->
+                        Cmd.batch
+                            [ Task.attempt PipelineFetched <|
+                                Concourse.Pipeline.fetchPipeline pid
+                            , Task.attempt (TopMsg TopBar.UserFetched) Concourse.User.fetchUser
+                            ]
+                )
 
         navIndex =
             1
@@ -102,6 +134,7 @@ init flags location =
             , notFoundImgSrc = flags.notFoundImgSrc
             , route = route
             , csrfToken = flags.csrfToken
+            , pipeline = Nothing
             }
 
         handleTokenCmd =
@@ -126,6 +159,31 @@ init flags location =
             , Cmd.map (TopMsg navIndex) topCmd
             ]
         )
+
+
+extractPidFromRoute : Routes.Route -> Maybe Concourse.PipelineIdentifier
+extractPidFromRoute route =
+    case route of
+        Routes.Build teamName pipelineName jobName buildName ->
+            Just { teamName = teamName, pipelineName = pipelineName }
+
+        Routes.Job teamName pipelineName jobName ->
+            Just { teamName = teamName, pipelineName = pipelineName }
+
+        Routes.Resource teamName pipelineName resourceName ->
+            Just { teamName = teamName, pipelineName = pipelineName }
+
+        Routes.OneOffBuild buildId ->
+            Nothing
+
+        Routes.Pipeline teamName pipelineName ->
+            Just { teamName = teamName, pipelineName = pipelineName }
+
+        Routes.Dashboard ->
+            Nothing
+
+        Routes.DashboardHd ->
+            Nothing
 
 
 locationMsg : Navigation.Location -> Msg
@@ -179,6 +237,63 @@ update msg model =
             else
                 ( model, Cmd.none )
 
+        LogOut ->
+            ( model, logOut )
+
+        LoggedOut (Ok _) ->
+            let
+                topModel =
+                    model.topModel
+            in
+                ( { model
+                    | topModel =
+                        { topModel
+                            | userState = TopBar.UserStateLoggedOut
+                            , pipeline = Nothing
+                        }
+                  }
+                , Navigation.newUrl "/"
+                )
+
+        LoggedOut (Err err) ->
+            flip always (Debug.log "failed to log out" err) <|
+                ( model, Cmd.none )
+
+        LogIn ->
+            let
+                topModel =
+                    model.topModel
+            in
+                ( { model | topModel = { topModel | pipeline = Nothing } }
+                , LoginRedirect.requestLoginRedirect ""
+                )
+
+        FetchPipeline pid ->
+            ( model
+            , Task.attempt PipelineFetched <|
+                Concourse.Pipeline.fetchPipeline pid
+            )
+
+        PipelineFetched (Err err) ->
+            case err of
+                Http.BadStatus { status } ->
+                    if status.code == 401 then
+                        ( model, LoginRedirect.requestLoginRedirect "" )
+                    else
+                        ( model, Cmd.none )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        PipelineFetched (Ok pipeline) ->
+            let
+                topModel =
+                    model.topModel
+            in
+                ( { model | topModel = { topModel | pipeline = Just pipeline } }
+                , Cmd.none
+                )
+
         TopMsg navIndex m ->
             if validNavIndex model.navIndex navIndex then
                 let
@@ -191,6 +306,11 @@ update msg model =
 
         Noop ->
             ( model, Cmd.none )
+
+
+logOut : Cmd Msg
+logOut =
+    Task.attempt LoggedOut Concourse.User.logOut
 
 
 validNavIndex : NavIndex -> NavIndex -> Bool
@@ -222,7 +342,24 @@ urlUpdate route model =
             if route == model.route then
                 ( model.topModel, Cmd.none )
             else
-                TopBar.urlUpdate route model.topModel
+                let
+                    pipelineIdentifier =
+                        pipelineIdentifierFromRouteOrModel route model
+                in
+                    ( { model
+                        | route = route
+                      }
+                    , case pipelineIdentifier of
+                        Nothing ->
+                            TopBar.fetchUser
+
+                        Just pid ->
+                            Cmd.batch
+                                [ Task.attempt PipelineFetched <|
+                                    Concourse.Pipeline.fetchPipeline pid
+                                , TopBar.fetchUser
+                                ]
+                    )
     in
         ( { model
             | navIndex = navIndex
@@ -256,7 +393,25 @@ view model =
         _ ->
             Html.div [ class "content-frame" ]
                 [ Html.div [ id "top-bar-app" ]
-                    [ Html.map (TopMsg model.navIndex) (TopBar.view model.topModel) ]
+                    [ Html.nav
+                        [ classList
+                            [ ( "module-topbar", True )
+                            , ( "top-bar", True )
+                            , ( "test", True )
+                            , ( "paused", Maybe.withDefault False <| Maybe.map .paused model.pipeline )
+                            ]
+                        ]
+                        [ Html.div
+                            [ classList [ ( "topbar-logo", True ) ] ]
+                            [ Html.a [ class "logo-image-link", href "/" ] [] ]
+                        , Html.ul [ class "groups" ] <| viewBreadcrumbs model.route.logical
+                        , Html.div [ class "topbar-login" ]
+                            [ Html.div [ class "topbar-user-info" ]
+                                [ viewUserState model.topModel.userState model.topModel.userMenuVisible
+                                ]
+                            ]
+                        ]
+                    ]
                 , Html.div [ class "bottom" ]
                     [ Html.div [ id "content" ]
                         [ Html.div [ id "subpage" ]
@@ -264,6 +419,103 @@ view model =
                         ]
                     ]
                 ]
+
+
+viewUserState : TopBar.UserState -> Bool -> Html Msg
+viewUserState userState userMenuVisible =
+    case userState of
+        TopBar.UserStateUnknown ->
+            Html.text ""
+
+        TopBar.UserStateLoggedOut ->
+            Html.div [ class "user-id", onClick LogIn ]
+                [ Html.a
+                    [ href "/sky/login"
+                    , Attributes.attribute "aria-label" "Log In"
+                    , class "login-button"
+                    ]
+                    [ Html.text "login"
+                    ]
+                ]
+
+        TopBar.UserStateLoggedIn user ->
+            Html.div [ class "user-info" ]
+                [ Html.div [ class "user-id", onClick TopMsg TopBar.ToggleUserMenu ]
+                    [ Html.text <|
+                        TopBar.userDisplayName user
+                    ]
+                , Html.div [ classList [ ( "user-menu", True ), ( "hidden", not userMenuVisible ) ], onClick LogOut ]
+                    [ Html.a
+                        [ Attributes.attribute "aria-label" "Log Out"
+                        ]
+                        [ Html.text "logout"
+                        ]
+                    ]
+                ]
+
+
+viewBreadcrumbs : Routes.Route -> List (Html Msg)
+viewBreadcrumbs route =
+    List.intersperse (Html.li [ class "nav-item" ] [ Html.text "/" ]) <|
+        case route of
+            Routes.Pipeline teamName pipelineName ->
+                [ viewBreadcrumbPipeline pipelineName route ]
+
+            Routes.Job teamName pipelineName jobName ->
+                [ viewBreadcrumbPipeline pipelineName <| Routes.Pipeline teamName pipelineName
+                , viewBreadcrumbJob jobName
+                ]
+
+            Routes.Build teamName pipelineName jobName buildName ->
+                [ viewBreadcrumbPipeline pipelineName <| Routes.Pipeline teamName pipelineName
+                , viewBreadcrumbJob jobName
+                ]
+
+            Routes.Resource teamName pipelineName resourceName ->
+                [ viewBreadcrumbPipeline pipelineName <| Routes.Pipeline teamName pipelineName
+                , viewBreadcrumbResource resourceName
+                ]
+
+            _ ->
+                []
+
+
+viewBreadcrumbPipeline : String -> Routes.Route -> Html Msg
+viewBreadcrumbPipeline pipelineName route =
+    let
+        url =
+            Routes.toString route
+    in
+        Html.li [ class "nav-item" ]
+            [ Html.a
+                [ StrictEvents.onLeftClick <| TopMsg TopBar.ResetToPipeline url
+                , href url
+                ]
+                [ Html.div [ class "breadcrumb-icon breadcrumb-pipeline-icon" ] []
+                , Html.text <| () decodeName pipelineName
+                ]
+            ]
+
+
+viewBreadcrumbJob : String -> Html Msg
+viewBreadcrumbJob name =
+    Html.li [ class "nav-item" ]
+        [ Html.div [ class "breadcrumb-icon breadcrumb-job-icon" ] []
+        , Html.text <| decodeName name
+        ]
+
+
+viewBreadcrumbResource : String -> Html Msg
+viewBreadcrumbResource name =
+    Html.li [ class "nav-item" ]
+        [ Html.div [ class "breadcrumb-icon breadcrumb-resource-icon" ] []
+        , Html.text <| decodeName name
+        ]
+
+
+decodeName : String -> String
+decodeName name =
+    Maybe.withDefault name (Http.decodeUri name)
 
 
 subscriptions : Model -> Sub Msg
@@ -280,9 +532,30 @@ subscriptions model =
             Sub.batch
                 [ newUrl NewUrl
                 , tokenReceived TokenReceived
-                , Sub.map (TopMsg model.navIndex) <| TopBar.subscriptions model.topModel
                 , Sub.map (SubMsg model.navIndex) <| SubPage.subscriptions model.subModel
+                , case pipelineIdentifierFromRouteOrModel model.route model of
+                    Nothing ->
+                        Sub.none
+
+                    Just pid ->
+                        Time.every (5 * Time.second) (always (FetchPipeline pid))
+                , Time.every (5 * Time.second) (TopMsg TopBar.FetchUser)
                 ]
+
+
+pipelineIdentifierFromRouteOrModel : Routes.ConcourseRoute -> Model -> Maybe Concourse.PipelineIdentifier
+pipelineIdentifierFromRouteOrModel route model =
+    case extractPidFromRoute route.logical of
+        Nothing ->
+            case model.pipeline of
+                Nothing ->
+                    Nothing
+
+                Just pipeline ->
+                    Just { teamName = pipeline.teamName, pipelineName = pipeline.name }
+
+        Just pidFromRoute ->
+            Just pidFromRoute
 
 
 routeMatchesModel : Routes.ConcourseRoute -> Model -> Bool
